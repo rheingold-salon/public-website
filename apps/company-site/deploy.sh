d05927e0ac7c1c7faf8b2a9bad03baa2c3006220
Nils Imdahl <imdahlnils@gmail.com>
2025-03-24 21:53:56 +0100

    more deployment stuff

--- apps/company-site/deploy.sh
+++ apps/company-site/deploy.sh
@@ -2,14 +2,13 @@
 echo "Pulling latest changes..."
 git pull
 
-cd $APP_DIR
+cd company-site/
 sudo docker-compose up --build -d
 
 # Check if Docker Compose started correctly
 if ! sudo docker-compose ps | grep "Up"; then
-  echo "Docker containers failed to start. Check logs with 'docker-compose logs'."
-  exit 1
+    echo "Docker containers failed to start. Check logs with 'docker-compose logs'."
+    exit 1
 fi
 
-echo "Deployment complete. Your Next.js app is now running. 
-Next.js is available at https://www.rheingold-salon.de.
+echo "Deployment complete. Your Next.js app is now running. Next.js is available at https://www.rheingold-salon.de."
