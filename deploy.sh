eac47a18fe8a754bfa4f48e57001c32c53809fc6
Nils Imdahl <imdahlnils@gmail.com>
2025-04-01 11:32:23 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -5,24 +5,24 @@ echo "Pulling latest changes..."
 git pull
 
 # Check if containers are already running
-if sudo docker-compose ps | grep "Up"; then
+if docker compose ps | grep "Up"; then
     echo "Existing containers are running. Shutting down old container..."
 
     # Pull new images without affecting the running containers
-    sudo docker-compose down
+    docker compose down
 fi
 
 echo "Starting new container"
-sudo docker-compose up -d --build
+docker compose up -d --build
 
 # Verify deployment
-if ! sudo docker-compose ps | grep "Up"; then
+if ! docker compose ps | grep "Up"; then
     echo "Docker containers failed to start. Check logs with 'docker-compose logs'."
     exit 1
 fi
 
 # Clean up any unused images and volumes to save space
 echo "Cleaning up unused Docker resources..."
-sudo docker system prune -f
+docker system prune -f
 
 echo "Deployment complete. Your Next.js app is now running. Next.js is available at https://www.rheingold-salon.de."
