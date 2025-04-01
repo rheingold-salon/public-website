bb6d96a63bbfeda1206123e6fe87b847fa2df0c1
Nils Imdahl <imdahlnils@gmail.com>
2025-04-01 15:53:18 +0200

    change remote host to www

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'neuewebsite.rheingold-salon.de',
+                hostname: 'www.rheingold-salon.de',
                 pathname: '/static/images/**',
                 port: '',
                 search: '',
